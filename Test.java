5cbc924480aec0ec49647a9ed757d853549d71b6
Joshua Bevers <infernezor@gmail.com>
2021-05-03 07:38:01 -0400

    setting up to start learning Java for InfoSys Bootcamp.

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test  
+{  
+    public static void main(String args[])  
+    {  
+        System.out.println("first statement.");  
+        System.out.println("second statement.");  
+        System.out.println("third statement.");  
+    }  
+}  
