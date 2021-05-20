1dd50911279cd79655a1c07be7d5cd6a7c3502af
Joshua Bevers <infernezor@gmail.com>
2021-05-20 14:19:35 -0400

    truncated Runner.

---
+++ oop/abstraction/abRunner.java
@@ -0,0 +1,21 @@
+package oop.abstraction;
+import java.util.ArrayList;
+import java.util.List;
+
+public class abRunner {
+
+    public static void RUNNER(){
+        car Mazda = new car("car", 1992, "Mazda", 4);
+        motocycle terry = new motocycle("Clairance", 1992, "GOd HIMSELF", "Granit");
+        List<vehical> peronalGarage = new ArrayList<vehical>();
+        garage parkingLot = new garage(peronalGarage);
+        parkingLot.addLocomotive(Mazda);
+        parkingLot.addLocomotive(terry);
+        parkingLot.printList();
+        parkingLot.removeLocomotive(Mazda);
+        parkingLot.printList();
+        parkingLot.addLocomotive(Mazda);
+        parkingLot.printList();
+    }
+    
+}
--- runner.java
+++ runner.java
@@ -1,8 +1,7 @@
 import arrays.arraylooping;
 import classes.book;
 import fizzbuzz.buzzy;
-import java.util.ArrayList;
-import java.util.List;
+import oop.abstraction.abRunner;
 import oop.Employee;
 import oop.abstraction.car;
 import oop.abstraction.garage;
@@ -29,18 +28,9 @@ public class runner {
     // Employee James = new Employee("James", 20, "Annoying", 52);
     // System.out.println(James.calculateWeeklySalary());
 
-    // oop.abstraction
-    car Mazda = new car("car", 1992, "Mazda", 4);
-    motocycle terry = new motocycle("Clairance", 1992, "GOd HIMSELF", "Granit");
-    List<vehical> peronalGarage = new ArrayList<vehical>();
-    garage parkingLot = new garage(peronalGarage);
-    parkingLot.addLocomotive(Mazda);
-    parkingLot.addLocomotive(terry);
-    parkingLot.printList();
-    parkingLot.removeLocomotive(Mazda);
-    parkingLot.printList();
-    parkingLot.addLocomotive(Mazda);
-    parkingLot.printList();
+    // oop.abstraction Here we begin new runner format to truncate code in the main runner.
+    abRunner.RUNNER();
+   
 
 
 
